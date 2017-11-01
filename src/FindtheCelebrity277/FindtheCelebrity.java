3e3229ecdaf37bc2622a69437b7739d7484ecded
DanielZhangLing <lingzseed@gmail.com>
2017-11-01 15:44:21 -0400

    fix candidate
    fix candidate

--- src/FindtheCelebrity277/FindtheCelebrity.java
+++ src/FindtheCelebrity277/FindtheCelebrity.java
@@ -4,17 +4,15 @@ public class FindtheCelebrity {
 
     public int findCelebrity(int n) {
         int candidate = 0;    	
-        // The first pass is to pick out the candidate. i doesn't know candidate, then switch candidate. 
+        // The first pass is to pick out the candidate. i doesn't know candidate, 
+        // then switch candidate. if exists only could be this guy
         for(int i = 1; i<n ; i++){
             if(knows(candidate,i))
                 candidate = i;
-        //doesn't check if candidate doesn't know former guys before switch him
-        }for(int i = 0; i<candidate; i++){
-            if(knows(candidate,i))
-                return -1;
+        //doesn't check if candidate doesn't know former guys or if former one knows candidates before switch him
         }
-        for(int i = 0;i<n; i++){
-            if(i!=candidate&&!knows(i,candidate))
+        for(int i = 0 ; i<n; i++){
+            if(i!= candidate&& (knows(candidate,i)||!knows(i,candidate)))
                 return -1;
         }
         return candidate;
