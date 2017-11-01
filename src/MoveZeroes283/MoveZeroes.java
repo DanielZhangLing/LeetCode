0d32de3c7d060e4fcad4613d4b2b24c53de5b0ac
Daniel Zhang <zhang.ling1@husky.neu.edu>
2017-11-01 14:04:02 -0400

    add
    add

--- src/MoveZeroes283/MoveZeroes.java
+++ src/MoveZeroes283/MoveZeroes.java
@@ -4,6 +4,19 @@ import java.util.Vector;
 
 public class MoveZeroes {
 
+	public void moveZeroes2(int[] nums) {
+		if (nums == null || nums.length == 0) return;
+
+		int insertPos = 0;
+		for (int num: nums) {
+			if (num != 0) nums[insertPos++] = num;
+		}
+
+		while (insertPos < nums.length) {
+			nums[insertPos++] = 0;
+		}
+	}
+	
     public int[] moveZeroes(int[] nums) {
         int count = 0;
         for(int i = 0; i<nums.length;i++){
@@ -21,18 +34,7 @@ public class MoveZeroes {
         return nums;
     }
 
-	public void moveZeroes2(int[] nums) {
-		if (nums == null || nums.length == 0) return;
 
-		int insertPos = 0;
-		for (int num: nums) {
-			if (num != 0) nums[insertPos++] = num;
-		}
-
-		while (insertPos < nums.length) {
-			nums[insertPos++] = 0;
-		}
-	}
 
 
 	public static void main(String[] args){
--- src/RemoveDuplicatesfromSortedArrayII80/RemoveDuplicatesfromSortedArrayII80.java
+++ src/RemoveDuplicatesfromSortedArrayII80/RemoveDuplicatesfromSortedArrayII80.java
@@ -10,6 +10,27 @@ public class RemoveDuplicatesfromSortedArrayII80 {
 		}
 	
 	
+	int removeDuplicates(int A[], int n, int k) {
+
+        if (n <= k) return n;
+
+        int i = 1, j = 1;
+        int cnt = 1;
+        while (j < n) {
+            if (A[j] != A[j-1]) {
+                cnt = 1;
+                A[i++] = A[j];
+            }
+            else {
+                if (cnt < k) {
+                    A[i++] = A[j];
+                    cnt++;
+                }
+            }
+            ++j;
+        }
+        return i;
+}
 	// trash
     public int removeDuplicates(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
