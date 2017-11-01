7d48f2e1c932b8f94efecb759dc8ea39f31fd2a6
DanielZhangLing <lingzseed@gmail.com>
2017-11-01 15:13:41 -0400

    comment
    comment

--- src/DecodeWays/DecodeWays91.java
+++ src/DecodeWays/DecodeWays91.java
@@ -12,10 +12,10 @@ public class DecodeWays91 {
         for(int i = 2; i <= n; i++) {
             int first = Integer.valueOf(s.substring(i-1, i));
             int second = Integer.valueOf(s.substring(i-2, i));
-            if(first >= 1 && first <= 9) {
+            if(first >= 1 && first <= 9) {// treat the new digit a single number
                dp[i] += dp[i-1];  
             }
-            if(second >= 10 && second <= 26) {
+            if(second >= 10 && second <= 26) {// treat the new digit combine with the last digit
                 dp[i] += dp[i-2];
             }
         }
