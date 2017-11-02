b33c5fd755dba3923c5dbe366225c98bb0991821
lingzseed <lingzseed@gmail.com>
2017-11-02 16:25:10 -0400

    add

--- src/SparseMatrixMultiplication311/SparseMatrixMultiplication.java
+++ src/SparseMatrixMultiplication311/SparseMatrixMultiplication.java
@@ -2,6 +2,33 @@ package SparseMatrixMultiplication311;
 
 public class SparseMatrixMultiplication {
 
+	//n^3
+    public int[][] multiply(int[][] A, int[][] B) {
+        if (A == null || A[0] == null || B == null || B[0] == null) return null;
+        int m = A.length, n = A[0].length, l = B[0].length;
+        int[][] C = new int[m][l];
+        Map<Integer, HashMap<Integer, Integer>> tableB = new HashMap<>();
+        
+        for(int k = 0; k < n; k++) {
+            tableB.put(k, new HashMap<Integer, Integer>());
+            for(int j = 0; j < l; j++) {
+                if (B[k][j] != 0){
+                    tableB.get(k).put(j, B[k][j]);
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int k = 0; k < n; k++) {
+                if (A[i][k] != 0){
+                    for (Integer j: tableB.get(k).keySet()) {
+                        C[i][j] += A[i][k] * tableB.get(k).get(j);
+                    }
+                }
+            }
+        }
+        return C;   
+    }
 	/*
 	 * 改变哪个先循环是一样的关键是把判断尽量拿到外面做这样提早省掉不必要的操作
 	 */
