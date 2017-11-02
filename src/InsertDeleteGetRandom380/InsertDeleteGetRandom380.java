cfaa97ddc24bfdae49048926e768c4b392d183c1
DanielZhangLing <lingzseed@gmail.com>
2017-11-01 23:01:21 -0400

    Update InsertDeleteGetRandom380.java

--- src/InsertDeleteGetRandom380/InsertDeleteGetRandom380.java
+++ src/InsertDeleteGetRandom380/InsertDeleteGetRandom380.java
@@ -1,49 +1,49 @@
 package InsertDeleteGetRandom380;
 
 public class InsertDeleteGetRandom380 {
-    ArrayList<Integer> nums;
-    HashMap<Integer, Integer> locs;
-    /** Initialize your data structure here. */
-    public RandomizedSet() {
-        nums = new ArrayList<Integer>();
-        locs = new HashMap<Integer, Integer>();
-    }
+//     ArrayList<Integer> nums;
+//     HashMap<Integer, Integer> locs;
+//     /** Initialize your data structure here. */
+//     public RandomizedSet() {
+//         nums = new ArrayList<Integer>();
+//         locs = new HashMap<Integer, Integer>();
+//     }
     
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    public boolean insert(int val) {
-        boolean contain = locs.containsKey(val);
-        if ( contain ) return false;
-        locs.put( val, nums.size());
-        nums.add(val);
-        return true;
-    }
+//     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+//     public boolean insert(int val) {
+//         boolean contain = locs.containsKey(val);
+//         if ( contain ) return false;
+//         locs.put( val, nums.size());
+//         nums.add(val);
+//         return true;
+//     }
     
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
-    public boolean remove(int val) {
-    	/*
-    	 * remember when we delete one item we actually switch
-    	 * the last item with this and then
-    	 * delete the last one to keep the size correctly
-    	 */
-        boolean contain = locs.containsKey(val);
-        if ( ! contain ) return false;
-        int loc = locs.get(val);
-        if (loc < nums.size() - 1 ) { // not the last one than swap the last one with this val
-            int lastone = nums.get(nums.size() - 1 );
-            nums.set( loc , lastone );
-            locs.put(lastone, loc);
-        }
-        locs.remove(val);
-        nums.remove(nums.size() - 1);
-        return true;
-    }
+//     /** Removes a value from the set. Returns true if the set contained the specified element. */
+//     public boolean remove(int val) {
+//     	/*
+//     	 * remember when we delete one item we actually switch
+//     	 * the last item with this and then
+//     	 * delete the last one to keep the size correctly
+//     	 */
+//         boolean contain = locs.containsKey(val);
+//         if ( ! contain ) return false;
+//         int loc = locs.get(val);
+//         if (loc < nums.size() - 1 ) { // not the last one than swap the last one with this val
+//             int lastone = nums.get(nums.size() - 1 );
+//             nums.set( loc , lastone );
+//             locs.put(lastone, loc);
+//         }
+//         locs.remove(val);
+//         nums.remove(nums.size() - 1);
+//         return true;
+//     }
     
-    /** Get a random element from the set. */
-    public int getRandom() {
-        return nums.get( new Random().nextInt(nums.size()) );
-    }
+//     /** Get a random element from the set. */
+//     public int getRandom() {
+//         return nums.get( new Random().nextInt(nums.size()) );
+//     }
     
-}
+// }
 
 class RandomizedSet {
 
