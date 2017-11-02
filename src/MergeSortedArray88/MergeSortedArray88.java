2ff940886862040447ddf188ec52f07b4b266865
DanielZhangLing <lingzseed@gmail.com>
2017-11-01 21:04:25 -0400

    Update MergeSortedArray88.java

--- src/MergeSortedArray88/MergeSortedArray88.java
+++ src/MergeSortedArray88/MergeSortedArray88.java
@@ -2,7 +2,7 @@ package MergeSortedArray88;
 
 public class MergeSortedArray88 {
 	/*
-	 * keypoint: add from last to the first
+	 * keypoint: add from last to the first or how can we get number from nums1
 	 */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m-1, j = n-1, k = m+n-1;
