a705eeac678e9dbcf8994cff584bc515bcefeef5
DanielZhangLing <lingzseed@gmail.com>
2017-11-14 14:51:10 -0500

    Update LetterCombinationsofaPhoneNumber.java

--- src/LetterCombinationsofaPhoneNumber17/LetterCombinationsofaPhoneNumber.java
+++ src/LetterCombinationsofaPhoneNumber17/LetterCombinationsofaPhoneNumber.java
@@ -30,26 +30,22 @@ public class LetterCombinationsofaPhoneNumber {
 	/*
 	 * 这里算是递归，每次加上一个数字
 	 */
-    public static List<String> letterCombinations(String digits) {
-        String digitletter[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        List<String> result = new ArrayList<String>();
-
-        if (digits.length()==0) return result;
-
-        result.add("");
-        for (int i=0; i<digits.length(); i++)
-            result = combine(digitletter[digits.charAt(i)-'0'],result);
-
-        return result;
-    }
-
-    public static List<String> combine(String digit, List<String> l) {
-        List<String> result = new ArrayList<String>();
-
-        for (int i=0; i<digit.length(); i++)
-            for (String x : l)
-                result.add(x+digit.charAt(i));
-
-        return result;
-    }
+    	private static final String[] KEYS = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    
+    	public List<String> letterCombinations(String digits) {
+    		List<String> ret = new LinkedList<String>();
+    		combination("", digits, 0, ret);
+    		return ret;
+    	}
+    
+    	private void combination(String prefix, String digits, int offset, List<String> ret) {
+    		if (offset >= digits.length()) {
+    			ret.add(prefix);
+    			return;
+    		}
+    		String letters = KEYS[(digits.charAt(offset) - '0')];
+    		for (int i = 0; i < letters.length(); i++) {
+    			combination(prefix + letters.charAt(i), digits, offset + 1, ret);
+    		}
+    	}
 }
