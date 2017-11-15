44308e61df0450acdeb76af56eddceb2c6d5a262
DanielZhangLing <lingzseed@gmail.com>
2017-11-14 20:43:03 -0500

    Update InorderSuccessorinBST285.java

--- src/InorderSuccessorinBST285/InorderSuccessorinBST285.java
+++ src/InorderSuccessorinBST285/InorderSuccessorinBST285.java
@@ -25,4 +25,27 @@ public class InorderSuccessorinBST285 {
         }
         return suc;
     }
+	
+	public TreeNode successor(TreeNode root, TreeNode p) {
+  if (root == null)
+    return null;
+
+  if (root.val <= p.val) {
+    return successor(root.right, p);
+  } else {
+    TreeNode left = successor(root.left, p);
+    return (left != null) ? left : root;
+  }
+}
+public TreeNode predecessor(TreeNode root, TreeNode p) {
+  if (root == null)
+    return null;
+
+  if (root.val >= p.val) {
+    return predecessor(root.left, p);
+  } else {
+    TreeNode right = predecessor(root.right, p);
+    return (right != null) ? right : root;
+  }
+}
 }
